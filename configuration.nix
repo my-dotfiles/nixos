6d73eac4f8ce196e5d71d0fb89acecafcbcda047
yurikon <h6606797@gmail.com>
2026-05-29 02:29:43 +0800

    add fcitx5

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,20 @@
     LC_TIME = "zh_CN.UTF-8";
   };
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        qt6Packages.fcitx5-chinese-addons
+        fcitx5-gtk
+	fcitx5-rime
+	fcitx5-mozc 
+     ];
+    };
+  };
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
