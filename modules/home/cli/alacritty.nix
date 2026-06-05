56024985cae7d412c1fe195c2bed56757c601e54
yurikon <h6606797@gmail.com>
2026-06-05 16:57:47 +0800

    alacrity: modify configuration

---
+++ modules/home/cli/alacritty.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.myHome.cli.alacritty;
+in
+{
+  options.myHome.cli.alacritty.enable = lib.mkEnableOption "Alacritty terminal emulator";
+
+  config = lib.mkIf cfg.enable {
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        window.blur = true;
+        window.opacity = 0.95;
+        font = {
+          size = 13;
+          normal.family = "Iosevka Nerd Font Mono";
+          offset.y = 1;
+        };
+      };
+    };
+
+    home.sessionVariables.TERMINAL = "alacritty";
+  };
+}
