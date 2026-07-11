1bb710e277350dedda94b365b7182f102a5996e9
yurikon <h6606797@gmail.com>
2026-07-11 14:26:11 +0800

    plasma: modify keyboard

---
+++ modules/home/desktop/plasma.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.myHome.desktop.plasma;
+in
+{
+  options.myHome.desktop.plasma.enable = lib.mkEnableOption "KDE Plasma user configuration";
+
+  config = lib.mkIf cfg.enable {
+    xdg.configFile."kxkbrc" = {
+      force = true;
+      text = ''
+        [Layout]
+        DisplayNames=
+        LayoutList=us
+        LayoutLoopCount=-1
+        Model=pc105
+        Options=ctrl:nocaps
+        ResetOldOptions=true
+        Use=true
+        VariantList=
+      '';
+    };
+  };
+}
